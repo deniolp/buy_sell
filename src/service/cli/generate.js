e66bec7dab601f4d1c9e2bb76c5b258bf385e39d
deniolp <deniolp@gmail.com>
2021-07-03 18:23:21 +0300

    Preparing module for generating offers

---
+++ src/service/cli/generate.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const fs = require(`fs`).promises;
+const chalk = require(`chalk`);
+
+const utils = require(`./../utils`);
+
+const DEFAULT_COUNT = 1;
+const FILE_NAME = `mocks.json`;
+
+const TITLES = [
+  `Продам книги Стивена Кинга`,
+  `Продам новую приставку Sony Playstation 5`,
+  `Продам отличную подборку фильмов на VHS`,
+  `Куплю антиквариат`,
+  `Куплю породистого кота`,
+];
+
+const SENTENCES = [
+  `Товар в отличном состоянии.`,
+  `Пользовались бережно и только по большим праздникам.`,
+  `Продаю с болью в сердце...`,
+  `Бонусом отдам все аксессуары.`,
+  `Даю недельную гарантию.`,
+  `Если товар не понравится — верну всё до последней копейки.`,
+  `Это настоящая находка для коллекционера!`,
+  `Если найдёте дешевле — сброшу цену.`,
+  `Таких предложений больше нет!`,
+  `При покупке с меня бесплатная доставка в черте города.`,
+];
+
+const CATEGORIES = [
+  `Книги`,
+  `Разное`,
+  `Посуда`,
+  `Игры`,
+  `Животные`,
+  `Журналы`,
+];
+
+const OfferType = {
+  OFFER: `offer`,
+  SALE: `sale`,
+};
+
+const SumRestrict = {
+  MIN: 1000,
+  MAX: 100000,
+};
+
+const PictureRestrict = {
+  MIN: 1,
+  MAX: 16,
+};
+
+const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.jpg`;
+
+const generateOffers = (count) => {
+  Array(count).fill({}).map(() => ({
+    category: [CATEGORIES[utils.getRandomNumber(0, CATEGORIES.length - 1)]],
+    description: utils.shuffle(SENTENCES).slice(1, 5).join(` `),
+    picture: getPictureFileName(utils.getRandomNumber(PictureRestrict.MIN, PictureRestrict.MAX)),
+    title: TITLES[utils.getRandomNumber(0, TITLES.length - 1)],
+    type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
+    sum: utils.getRandomNumber(SumRestrict.MIN, SumRestrict.MAX)
+  }));
+};
+
+module.exports = {
+  name: `--generate`,
+  async run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const content = JSON.stringify(generateOffers(countOffer));
+
+    try {
+      await fs.writeFile(FILE_NAME, content);
+      console.log(chalk.green(`Operation success. File created.`));
+    } catch (error) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
+  }
+};
