b753b87e8032a8110a44c20323a58037d88aee62
deniolp <deniolp@gmail.com>
2021-07-05 17:48:28 +0300

    Creating templates for pages

--- src/express/index.js
+++ src/express/index.js
@@ -2,6 +2,7 @@
 
 const express = require(`express`);
 const path = require(`path`);
+const chalk = require(`chalk`);
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
@@ -18,5 +19,15 @@ app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 
+app.use((req, res) => {
+  console.error(chalk.red(`Страница не найдена`));
+  res.status(404).render(`errors/404`, {title: `Страница не найдена`});
+});
+
+app.use((err, req, res, _next) => {
+  console.error(chalk.red(`Ошибка на сервере: ${err}`));
+  res.status(err.status || 500);
+  res.render(`errors/500`, {title: `Ошибка сервера`});
+});
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`));
--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -4,9 +4,17 @@ const {Router} = require(`express`);
 
 const offersRouter = new Router();
 
-offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
-offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id ${req.params.id}`));
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id ${req.params.id}`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id ${req.params.id}`));
+offersRouter.get(`/add`, (req, res) => {
+  res.render(`new-offer`, {});
+});
+offersRouter.get(`/:id`, (req, res) => {
+  res.render(`offer`, {});
+});
+offersRouter.get(`/category/:id`, (req, res) => {
+  res.render(`category`, {});
+});
+offersRouter.get(`/edit/:id`, (req, res) => {
+  res.render(`offer-edit`, {});
+});
 
 module.exports = offersRouter;
