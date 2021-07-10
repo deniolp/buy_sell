071438084fc4487bdb28777ce33def3b562df138
deniolp <deniolp@gmail.com>
2021-07-10 18:55:16 +0300

    Making all routes

--- src/service/api/offers.js
+++ src/service/api/offers.js
@@ -2,18 +2,101 @@
 
 const {Router} = require(`express`);
 
-const getMockData = require(`../lib/get-mock-data`);
+const offerValidator = require(`../middlewares/offer-validator`);
+const offerExist = require(`../middlewares/offer-exists`);
+const commentValidator = require(`../middlewares/comment-validator`);
 const {HttpCode} = require(`../../constants`);
 
-const offersRouter = new Router();
+const router = new Router();
 
-offersRouter.get(`/`, async (req, res) => {
-  try {
-    const mocks = await getMockData();
-    res.json(mocks);
-  } catch (err) {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
-  }
-});
+module.exports = (app, service, commentService) => {
+  app.use(`/offers`, router);
 
-module.exports = offersRouter;
+  router.get(`/`, async (req, res) => {
+    try {
+      const offers = await service.findAll();
+      res.status(HttpCode.OK)
+      .json(offers);
+    } catch (err) {
+      res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+    }
+  });
+
+  router.get(`/:offerId`, (req, res) => {
+    const {offerId} = req.params;
+    const offer = service.findOne(offerId);
+
+    if (!offer) {
+      return res.status(HttpCode.NOT_FOUND)
+        .send(`Not found with ${offerId}`);
+    }
+
+    return res.status(HttpCode.OK)
+        .json(offer);
+  });
+
+  router.post(`/`, offerValidator, (req, res) => {
+    const offer = service.create(req.body);
+
+    return res.status(HttpCode.CREATED)
+      .json(offer);
+  });
+
+  router.put(`/:offerId`, offerValidator, (req, res) => {
+    const {offerId} = req.params;
+    const existOffer = service.findOne(offerId);
+
+    if (!existOffer) {
+      return res.status(HttpCode.NOT_FOUND)
+        .send(`Not found with ${offerId}`);
+    }
+
+    const updatedOffer = service.update(offerId, req.body);
+
+    return res.status(HttpCode.OK)
+      .json(updatedOffer);
+  });
+
+  router.delete(`/:offerId`, (req, res) => {
+    const {offerId} = req.params;
+    const offer = service.drop(offerId);
+
+    if (!offer) {
+      return res.status(HttpCode.NOT_FOUND)
+        .send(`Offer not found`);
+    }
+
+    return res.status(HttpCode.OK)
+      .json(offer);
+  });
+
+  router.get(`/:offerId/comments`, offerExist(service), (req, res) => {
+    const {offer} = res.locals;
+    const comments = commentService.findAll(offer);
+
+    res.status(HttpCode.OK)
+      .json(comments);
+  });
+
+  router.delete(`/:offerId/comments/:commentId`, offerExist(service), (req, res) => {
+    const {offer} = res.locals;
+    const {commentId} = req.params;
+    const deletedComment = commentService.drop(offer, commentId);
+
+    if (!deletedComment) {
+      return res.status(HttpCode.NOT_FOUND)
+        .send(`Comment not found`);
+    }
+
+    return res.status(HttpCode.OK)
+      .json(deletedComment);
+  });
+
+  router.post(`/:offerId/comments`, [offerExist(service), commentValidator], (req, res) => {
+    const {offer} = res.locals;
+    const comment = commentService.create(offer, req.body);
+
+    return res.status(HttpCode.CREATED)
+      .json(comment);
+  });
+};
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -3,14 +3,25 @@
 const express = require(`express`);
 const chalk = require(`chalk`);
 
-const offersRoutes = require(`../api/offers`);
+const routes = require(`../api`);
+const {HttpCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 
 const app = express();
 app.use(express.json());
 
-app.use(`/offers`, offersRoutes);
+app.use(`/api`, routes);
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(`Page not found`);
+  console.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  console.error(`An error occured on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
