cdf32acf039704fb931ababd88b5c9d57cfa4077
deniolp <deniolp@gmail.com>
2021-07-14 17:49:12 +0300

    Using api in one of offers-routes

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -2,7 +2,10 @@
 
 const {Router} = require(`express`);
 
+const {getAPI} = require(`../api`);
+
 const offersRouter = new Router();
+const api = getAPI();
 
 offersRouter.get(`/add`, (req, res) => {
   res.render(`new-offer`, {});
@@ -13,8 +16,13 @@ offersRouter.get(`/:id`, (req, res) => {
 offersRouter.get(`/category/:id`, (req, res) => {
   res.render(`category`, {});
 });
-offersRouter.get(`/edit/:id`, (req, res) => {
-  res.render(`offer-edit`, {});
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`offer-edit`, {offer, categories});
 });
 
 module.exports = offersRouter;
