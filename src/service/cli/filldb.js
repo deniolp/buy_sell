ff1e613b09c4954216926daf2970fbe2e5d794d8
deniolp <deniolp@gmail.com>
2021-08-03 14:49:39 +0300

    Moving init-db block to sepate module

--- src/service/cli/filldb.js
+++ src/service/cli/filldb.js
@@ -3,8 +3,7 @@
 const fs = require(`fs`).promises;
 
 const sequelize = require(`../lib/sequelize`);
-const defineModels = require(`../models`);
-const Aliase = require(`../models/aliase`);
+const initDatabase = require(`../lib/init-db`);
 const {getLogger} = require(`../lib/logger`);
 const utils = require(`../utils`);
 const {
@@ -84,52 +83,6 @@ module.exports = {
     const categories = await utils.readContent(`categories`);
     const offers = generateOffers(countOffer, mockData);
 
-    try {
-      const {Category, Offer, User} = defineModels(sequelize);
-      const result = await sequelize.sync({force: true});
-      logger.info(`Successfully created ${result.config.database} database`);
-
-      const categoryModels = await Category.bulkCreate(
-          categories.map((item) => ({
-            title: item,
-            picture: `picture.png`,
-          }))
-      );
-
-      const categoryIdByName = categoryModels.reduce((acc, next) => ({
-        [next.title]: next.id,
-        ...acc
-      }), {});
-
-      const userModels = await User.bulkCreate(users, {include: [Aliase.OFFERS, Aliase.COMMENTS]});
-
-      const userIdByEmail = userModels.reduce((acc, next) => ({
-        [next.email]: next.id,
-        ...acc
-      }), {});
-
-      offers.forEach((offer) => {
-        offer.userId = userIdByEmail[offer.user];
-        offer.comments.forEach((comment) => {
-          comment.userId = userIdByEmail[comment.user];
-        });
-      });
-
-      const offerPromises = offers.map(async (offer) => {
-        const offerModel = await Offer.create(offer, {include: [Aliase.COMMENTS]});
-        await offerModel.addCategories(
-            offer.categories.map(
-                (title) => categoryIdByName[title]
-            )
-        );
-      });
-
-      await Promise.all(offerPromises);
-
-      logger.info(`Successfully filled ${result.config.database} database`);
-      sequelize.close();
-    } catch (error) {
-      logger.error(error);
-    }
+    return initDatabase(sequelize, {offers, categories, users});
   }
 };
---
+++ src/service/lib/init-db.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const defineModels = require(`../models`);
+const Aliase = require(`../models/aliase`);
+const {getLogger} = require(`../lib/logger`);
+
+const logger = getLogger({name: `init-db`});
+
+module.exports = async (sequelize, {categories, offers, users}) => {
+  try {
+    const {Category, Offer, User} = defineModels(sequelize);
+    const result = await sequelize.sync({force: true});
+    logger.info(`Successfully created ${result.config.database} database`);
+
+    const categoryModels = await Category.bulkCreate(
+        categories.map((item) => ({
+          title: item,
+          picture: `picture.png`,
+        }))
+    );
+
+    const categoryIdByName = categoryModels.reduce((acc, next) => ({
+      [next.title]: next.id,
+      ...acc
+    }), {});
+
+    const userModels = await User.bulkCreate(users, {include: [Aliase.OFFERS, Aliase.COMMENTS]});
+
+    const userIdByEmail = userModels.reduce((acc, next) => ({
+      [next.email]: next.id,
+      ...acc
+    }), {});
+
+    offers.forEach((offer) => {
+      offer.userId = userIdByEmail[offer.user];
+      offer.comments.forEach((comment) => {
+        comment.userId = userIdByEmail[comment.user];
+      });
+    });
+
+    const offerPromises = offers.map(async (offer) => {
+      const offerModel = await Offer.create(offer, {include: [Aliase.COMMENTS]});
+      await offerModel.addCategories(
+          offer.categories.map(
+              (title) => categoryIdByName[title]
+          )
+      );
+    });
+    await Promise.all(offerPromises);
+
+    logger.info(`Successfully filled ${result.config.database} database`);
+    sequelize.close();
+  } catch (error) {
+    logger.error(error);
+  }
+};
