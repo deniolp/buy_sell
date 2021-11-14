3f503a8c80a45aa6d32ecf207a9706812d0cfad9
deniolp <deniolp@gmail.com>
2021-11-14 15:46:51 +0300

    Adding couple of lessons

---
+++ src/lessons/emit/http.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require(`http`);
+
+const port = 8000;
+
+const onClientConnect = (request, response) => {
+  const userAgent = request.headers[`user-agent`];
+  response.end(`Hello, ${userAgent}`);
+};
+
+const httpServer = http.createServer(onClientConnect);
+
+httpServer.on(`listening`, () => console.log(`Принимаю подключения на ${port}`));
+httpServer.on(`connection`, (socket) => console.log(`Подключился новый клиент: ${socket.remoteAddress}`));
+httpServer.listen(port);
